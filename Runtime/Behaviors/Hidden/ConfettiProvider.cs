c5197e3a2ce920cb8559421b22679dbb7b7f7f17
TomWim <samo.miw@gmail.com>
2021-03-18 14:30:22 +0100

    feat: add more seasons

--- Runtime/Behaviors/Hidden/ConfettiProvider.cs
+++ Runtime/Behaviors/Hidden/ConfettiProvider.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using UnityEngine;
 
-public class ConfettiProvider : MonoBehaviour
+public static class ConfettiProvider
 {
     public const string DefaultConfettiPrefabPath = "Prefabs/InnoactiveConfettiMachine";
     
-    private static readonly Season[] Seasons = new[]
+    private static readonly Season[] Seasons =
     {
-        new Season("Valentines Day", "", new DateTime(DateTime.Today.Year, 2, 14)),
-        new Season("St. Patrick's Day", "Prefabs/StPatricksDayConfettiMachine", new DateTime(DateTime.Today.Year, 3, 17)),
-        new Season("Christmas", "", new DateTime(DateTime.Today.Year, 12, 12), new DateTime(DateTime.Today.Year, 12, 25)),
-        new Season("Halloween", "", new DateTime(DateTime.Today.Year, 10, 30))
+        new Season("Valentines Day", "Confetti/Hidden/Prefabs/ValentinesDayConfettiMachine", new DateTime(DateTime.Today.Year, 2, 14)),
+        new Season("St. Patrick's Day", "Confetti/Hidden/Prefabs/StPatricksDayConfettiMachine", new DateTime(DateTime.Today.Year, 3, 17)),
+        new Season("Christmas", "Confetti/Hidden/Prefabs/XmasConfettiMachine", new DateTime(DateTime.Today.Year, 12, 12), new DateTime(DateTime.Today.Year, 12, 25)),
+        new Season("Halloween", "Confetti/Hidden/Prefabs/HalloweenConfettiMachine", new DateTime(DateTime.Today.Year, 10, 31))
     };
 
     private class Season
